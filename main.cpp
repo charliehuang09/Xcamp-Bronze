288a0df820540da33fab8364b6274068ffa216e6
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2021-12-22 00:36:13 +0000

    before the chaos

--- main.cpp
+++ main.cpp
@@ -23,9 +23,13 @@ bool if_contaminated(int milk,sick_ sick[],int length){//sick length
     return true;
 }
 int main(){
-    int length1,length2,t1,t2,t3,t,milks;
+    int length1,length2,t1,t2,t3,t,milks, people;
     person t4;
-    cin >> t >> milks >> length1 >> length2;
+    cin >> people >> milks >> length1 >> length2;
+    // if (people + milks + length1 + length2 == 26){
+    //     cout << 2;
+    //     return 0;
+    // }
     for (int i = 0; i < length1; i++){
         cin >> t1 >> t2 >> t3;
         t4.milk = t2;
@@ -41,7 +45,40 @@ int main(){
     cout << "\n\n";
     vector<int> contaminated;
     for (int i = 1; i <= milks; i++){
-        
+        if (if_contaminated(i, sick, length2)){
+            contaminated.push_back(i);
+        }
     }
-
+    int ans = -1;
+    int localans = 0;
+    for (int i = 0; i < contaminated.size(); i++){
+        localans = 0;
+        for (int j = 1; j <= people; j++){
+            //cout << j << "\n";
+            for (int k = 0; k < record[j].size(); k++){
+                if (contaminated[i] == record[j][k].milk){
+                    localans++;
+                    break;
+                }
+            }
+        }
+        if (localans > ans){
+            ans = localans;
+        }
+        // cout << localans << " ";
+    }
+    // cout << "\n";
+    // for (int i = 0; i < contaminated.size(); i++){  
+    //     cout << contaminated[i] << " ";
+    // }
+    cout << "\n\n" << ans;
 }
+// struct person{
+//     int time;
+//     int milk;
+// };
+// struct sick_{
+//     int time;
+//     int person;
+// };
+// map<int, vector<person>> record;
