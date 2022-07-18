11af5df6f8cf9dd7a2760046bc8590379998c66d
charlie <charliehuang09@gmail.com>
2022-07-18 12:20:56 -0700

    Appending Mex

---
+++ Completed/Appending Mex.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//int main(){
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    int length;
+//    cin >> length;
+//    int biggest = -1;
+//    int input;
+//    for (int i = 0; i < length; i++){
+//        cin >> input;
+//        if (input > biggest + 1){
+//            cout << i + 1;
+//            return 0;
+//        }
+//        biggest = max(biggest, input);
+//    }
+//    cout << -1;
+//
+//}
