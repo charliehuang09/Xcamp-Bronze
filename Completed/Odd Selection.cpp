cfe06f8f0db4d9df935fed2cf4e0e792f8383442
charlie <charliehuang09@gmail.com>
2022-07-19 13:34:39 -0700

    Odd Selection

---
+++ Completed/Odd Selection.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//void solve(){
+//    int length, x;
+//    cin >> length >> x;
+//    int even = 0;
+//    int odd = 0;
+//    int t;
+//    for (int i = 0; i < length; i++){
+//        cin >> t;
+//        if (t % 2 == 0){
+//            even++;
+//        }
+//        else{
+//            odd++;
+//        }
+//    }
+//
+//}
+//int main(){
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    int length;
+//    cin >> length;
+//    for (int i = 0;i < length; i++){
+//        solve();
+//    }
+//}
