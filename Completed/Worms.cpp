cdc02b7dd96dfc55b987c431555bd5def5d8056e
charlie <charliehuang09@gmail.com>
2022-07-19 14:41:22 -0700

    Worms

---
+++ Completed/Worms.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//int main(){
+//
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    int length, t;
+//    cin >> length;
+//    vector<int> input;
+//    for (int i = 0; i < length; i++){
+//        cin >> t;
+//        for (int j = 0; j < t; j++){
+//            input.push_back(i + 1);
+//        }
+//    }
+//
+//    int size;
+//    cin >> size;
+//    for (int i = 0; i < size; i++){
+//        cin >> t;
+//        cout << input[t - 1] << "\n";
+//    }
+//
+//}
