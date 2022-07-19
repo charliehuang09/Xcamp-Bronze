4a0cf61eb3c21959d8f0fd00130393a8d7267eb4
charlie <charliehuang09@gmail.com>
2022-07-19 10:46:42 -0700

    help

--- Completed/Pashmak and Garden.cpp
+++ Completed/Pashmak and Garden.cpp
@@ -11,29 +11,95 @@ struct coord{
     int y;
 };
 
-string get_type(coord input){
-    if (input.x == 0 && input.y == 0){
-        return "bl";
-    }
-    if (input.x == 0){
-        return "tl";
-    }
-    if (input.y == 0){
-        return "br";
-    }
-    if (input.x == input.y){
-        return "tr";
-    }
-    cout << -1;
-    exit(0);
-}
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    coord output;
     coord input1, input2;
     cin >> input1.x >> input1.y >> input2.x >> input2.y;
-    string a = get_type(input1);
-    string b = get_type(input2);
+    set<int> coords;
+    coords.insert(input1.x);
+    coords.insert(input2.x);
+    coords.insert(input1.y);
+    coords.insert(input2.y);
+    coords.erase(0);
+    coords.erase(0);
+
+    if (input1.x - input2.x != input1.y - input2.y && input1.x != input2.x && input1.y != input2.y){
+        cout << -1;
+        return 0;
+    }
+
+    if (coords.size() > 1){
+        if (coords.size() == 2){
+            int t1 = *(coords.begin());
+            coords.erase(t1);
+            int t2 = *(coords.begin());
+            int bottom = min(t1, t2);
+            int top = max(t1, t2);
+            output.x = bottom;
+            output.y = bottom;
+            if (output.x != input1.x || output.y != input1.y){
+                if (output.x != input2.x || output.y != input2.y){
+                    cout << output.x << " " << output.y << " ";
+                }
+            }
+            output.x = bottom;
+            output.y = top;
+            if (output.x != input1.x || output.y != input1.y){
+                if (output.x != input2.x || output.y != input2.y){
+                    cout << output.x << " " << output.y << " ";
+                }
+            }
+            output.x = top;
+            output.y = bottom;
+            if (output.x != input1.x || output.y != input1.y){
+                if (output.x != input2.x || output.y != input2.y){
+                    cout << output.x << " " << output.y << " ";
+                }
+            }
+            output.x = top;
+            output.y = top;
+            if (output.x != input1.x || output.y != input1.y){
+                if (output.x != input2.x || output.y != input2.y){
+                    cout << output.x << " " << output.y << " ";
+                }
+            }
+            return 0;
+        }
+        cout << -1;
+        return 0;
+    }
+
+    int value = *(coords.begin());
+    output.x = 0;
+    output.y = value;
+    if (output.x != input1.x || output.y != input1.y){
+        if (output.x != input2.x || output.y != input2.y){
+            cout << output.x << " " << output.y << " ";
+        }
+    }
+    output.x = 0;
+    output.y = 0;
+    if (output.x != input1.x || output.y != input1.y){
+        if (output.x != input2.x || output.y != input2.y){
+            cout << output.x << " " << output.y << " ";
+        }
+    }
+    output.x = value;
+    output.y = 0;
+    if (output.x != input1.x || output.y != input1.y){
+        if (output.x != input2.x || output.y != input2.y){
+            cout << output.x << " " << output.y << " ";
+        }
+    }
+    output.x = value;
+    output.y = value;
+    if (output.x != input1.x || output.y != input1.y){
+        if (output.x != input2.x || output.y != input2.y){
+            cout << output.x << " " << output.y << " ";
+        }
+    }
+
 
 }
