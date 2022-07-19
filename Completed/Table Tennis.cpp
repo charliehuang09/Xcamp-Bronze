fd5429e77e20b2a97da82f88717524bf249d9426
charlie <charliehuang09@gmail.com>
2022-07-19 15:12:50 -0700

    No more TLE Table Tennis

--- Completed/Table Tennis.cpp
+++ Completed/Table Tennis.cpp
@@ -1,41 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <algorithm>
-using namespace std;
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int length, k, t;
-    cin >> length >> k;
-    queue<int> q;
-    for (int i = 0; i < length; i++){
-        cin >> t;
-        q.push(t);
-    }
-//    int i = 0;
-    int streak = 0;
-    int current_player = -1;
-    while (true){
-//        i++;
-//        cout << i << "\n";
-        int challenger = q.front();
-        if (current_player > challenger){
-            streak++;
-            q.pop();
-            q.push(challenger);
-            if (streak == k){
-                cout << current_player;
-                return 0;
-            }
-        }
-        else{
-            streak = 1;
-            current_player = challenger;
-            q.pop();
-            q.push(challenger);
-        }
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//int main(){
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    long long int length, k, t;
+//    cin >> length >> k >> t;
+//    long long int maximum = t;
+//    int king = t;
+//    long long int streak = 0;
+//    for (int i = 1; i < length; i++){
+//        cin >> t;
+//        maximum = max(maximum, t);
+//        if (king > t){
+//            streak++;
+//            if (streak == k){
+//                cout << king;
+//                return 0;
+//            }
+//        }
+//        else{
+//            streak = 1;
+//            king = t;
+//        }
+//    }
+//    cout << maximum;
+//}
