27e18fb082c11104594adcaacc49620de2cf2623
charlie <charliehuang09@gmail.com>
2022-07-24 15:49:13 -0700

    Reverse and Concatenate

--- Incompleted/Reverse and Concatenate.cpp
+++ Incompleted/Reverse and Concatenate.cpp
@@ -1,43 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <cstring>
-#include <map>
-#include <queue>
-#include <algorithm>
-using namespace std;
-
-set<string> s;
-
-void dfs(string input, int k){
-    if (k == 0){
-        s.insert(input);
-        return;
-    }
-    string t = input;
-    reverse(input.begin(), input.end());
-    dfs(input + t, k - 1);
-    dfs(t + input, k - 1);
-}
-
-void solve(){
-    int length, k;
-    string input;
-    cin >> length >> k;
-    cin >> input;
-    s.clear();
-    dfs(input, k);
-    cout << s.size() << "\n";
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int length;
-    cin >> length;
-    for (int i = 0; i < length; i++){
-        solve();
-    }
-
-}
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <cstring>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//
+//set<string> s;
+//
+//
+//void solve(){
+//    int length, k;
+//    string input, reversed;
+//    cin >> length >> k;
+//    cin >> input;
+//    reversed = input;
+//    reverse(reversed.begin(), reversed.end());
+//    if (reversed == input){
+//        cout << "1\n";
+//        return;
+//    }
+//    else{
+//        if (k == 0){
+//            cout << "1\n";
+//            return;
+//        }
+//        cout << "2\n";
+//    }
+//
+//}
+//
+//int main(){
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//
+//    int length;
+//    cin >> length;
+//    for (int i = 0; i < length; i++){
+//        solve();
+//    }
+//
+//}
