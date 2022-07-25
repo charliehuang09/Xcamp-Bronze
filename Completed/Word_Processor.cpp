eacf5069fbbb6ed52e5fc2641455d8c6f28a63a9
charlie <charliehuang09@gmail.com>
2022-07-25 10:31:02 -0700

    Word_Processor

---
+++ Completed/Word_Processor.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//#include <vector>
+//#include <set>
+//#include <cstring>
+//#include <map>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//int main(){
+////    freopen("word.in", "r", stdin);
+////    freopen("word.out", "w", stdout);
+//    int length;
+//    int size;
+//    cin >> length >> size;
+//    int size_of_word = 0;
+//    string input;
+//
+//    for (int i = 0; i < length; i++){
+//        cin >> input;
+//        if (input.size() + size_of_word > size){
+//            size_of_word = input.size();
+//            cout << "\n";
+//            cout << input;
+//        }
+//        else{
+//            size_of_word += input.size();
+//            if (i != 0){
+//                cout << " ";
+//            }
+//            cout << input;
+//        }
+//    }
+//
+//}
